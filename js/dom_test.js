60c9f2ae9086d1b1e88e9aacb8046dd43b4b9b05
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-05-25 16:38:08 +0600

    dom example are display here

--- js/dom_test.js
+++ js/dom_test.js
@@ -1,3 +1,6 @@
+/* 
+    IC URL : https://interactivecares.com/lessons/286?id=3820
+ */
 // console.log("hello to do list");
 console.log(document);
 // console.log(typeof document);
@@ -15,6 +18,36 @@ console.log(all_collections);
 let header = document.getElementById("header"); // find element by id.
 console.log(header);
 console.dir(header);
+//header.textContent = "DOM Tutorial"; // display as css style written in the style sheet
+//header.innerText = "DOM Tutorial to do"; // display as it is written in js
+//header.style.color = "white"; // change the color
+//header.style.backgroundColor = "red"; // change the background. use upper case letter for remove (-)
+/* 
+    in css we written background-color
+    in js DOM we written backgroundColor
+*/
 
-let items = document.getElementsByClassName("item"); // find elements by class name
+let item = document.getElementsByClassName("item"); // find elements by class name
+console.log(item);
+
+let items = document.querySelector("#items"); // use of query selector
 console.log(items);
+console.dir(items);
+
+let parent = document.querySelector("#items");
+console.log(parent.children); // use of parent children relation
+console.log(parent.parentElement); // to select parent element of the selected query tag
+
+let upper_element = parent.closest(".container");
+console.log(upper_element); // to select nearest tag of the variable.
+
+let previous_element = parent.previousElementSibling; // to select immediate upper level tag
+console.log(previous_element);
+
+let next_element = document.querySelector(".todo-list"); // to select immediate next level tag
+console.log(next_element.nextElementSibling);
+/* to select all queryselectorAll use for loop */
+/* let all_item = document.querySelectorAll(".item");
+for (let i = 0; i < all_item.length; i++) {
+  all_item[i].style.color = "red";
+} */
